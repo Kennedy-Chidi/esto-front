e1911205acd3044e9d554f70ad6083dc11ce96c9
Kennedy-Chidi <kennedychidi3@gmail.com>
2023-08-13 03:00:05 +0100

    good

---
+++ static/script/smartSupp.js
@@ -0,0 +1,18 @@
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = "54843aced33b6bceccbd4f22b85157a318e9bc8b";
+window.smartsupp ||
+  (function (d) {
+    var s,
+      c,
+      o = (smartsupp = function () {
+        o._.push(arguments);
+      });
+    o._ = [];
+    s = d.getElementsByTagName("script")[0];
+    c = d.createElement("script");
+    c.type = "text/javascript";
+    c.charset = "utf-8";
+    c.async = true;
+    c.src = "https://www.smartsuppchat.com/loader.js?";
+    s.parentNode.insertBefore(c, s);
+  })(document);
